26699cb7088da1f2fcbb55a23b9c7c22502b651a
Fuzail Ahmad <fuzaildbsj@gmail.com>
2024-11-30 23:42:17 +0530

    30-11-20204 23:42 PM Good Night

--- script.js
+++ script.js
@@ -9,29 +9,29 @@ async function getSongs() {
     let songs = []
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
-        if(element.href.endsWith(".mp3")){
+        if (element.href.endsWith(".mp3")) {
             songs.push(element.href.split("/songs/")[1])
         }
     }
     return songs
 }
 
-async function main(){
+async function main() {
+    
     let songs = await getSongs()
-    console.log(songs)
 
-    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0];
     for (const song of songs) {
-        songUL.innerHTML = songUL.innerHTML + `<li>${song.replaceAll("%20"," ")}</li>`;
+        songUL.innerHTML = songUL.innerHTML + `<li><img class="invert" src="./img/music.svg" alt="">
+        <div class="info">
+            <div>${song.replaceAll("%20", " ")}</div>
+            <div>Fuzail Ahmad</div>
+        </div>
+        <div class="playnow">
+            <span>Play Now</span>
+            <img class="invert" src="./img/play.svg" alt="">
+        </div></li>`;
     }
-
-    // Play the first song
-    var audio = new Audio(songs[0]);
-    audio.play();
-
-    audio.addEventListener("loadeddata", () => {
-        console.log(audio.duration, audio.currentSrc, audio.currentTime)
-    });
 }
 
 main()
